7a0a8f9019d05027a7f27e7776615519ff3606a6
Kagiso Nhlapo <130784383+Boemokg@users.noreply.github.com>
2024-04-02 15:42:36 +0200

    Add files via upload

---
+++ Builder.R
@@ -0,0 +1,24 @@
+## This is from this website: https://www.shilaan.com/post/building-your-website-using-r-blogdown/
+# Table of Contents
+# Prerequisites
+# Step 3: Create website with {blogdown}
+# Step 4: Push ⬆︎ to GitHub
+# Step 5: Deploy site with Netlify
+# Step 6: Customize your site with Wowchemy 🎨
+# Personal information
+# Widgets
+# Menu
+# Theme
+# Website icon
+# Step 7: Write your first post ✍
+
+####   Get packages from libraries ####
+
+install.packages("blogdown") # install the blogdown package
+library(blogdown) # load blogdown
+
+
+####   Create website with {blogdown} ####
+
+new_site(theme = "wowchemy/starter-academic") # create your website!
+
